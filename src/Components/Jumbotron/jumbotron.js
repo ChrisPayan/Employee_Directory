b88937fc92c882e18d88218ce4f7a7eca5d0d711
Christian Payan <payanstocks@gmail.com>
2021-05-14 14:24:28 -0700

    Added jumbotron jsx

---
+++ src/Components/Jumbotron/jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Employee Directory</h1>
+                <p className="lead">Click on what ever you want it wont do shiiiiiiiiiiiiiit</p>
+            </div>
+        </div>
+    )
+
+}
+
+export default Jumbotron;
