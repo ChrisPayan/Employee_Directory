dbb139a3a95c502966140590da0b875cfd484077
Christian Payan <payanstocks@gmail.com>
2021-05-15 22:37:34 -0700

    edited method of setting state from API and how table is rendered

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
+import React, { useState, useEffect } from "react";
 import Jumbotron from "./Components/Jumbotron/jumbotron";
 import SearchBar from "./Components/SearchBar/searchbar";
 import API from "./Util/API";
 
 function App() {
-  console.log(API.search());
+
+  const [employees, setEmployees] = useState({
+    users: [],
+    filteredUsers: [],
+    search: "",
+  });
+  console.log(employees.users);
+
+  const handleInputChange = (event) => {
+    const input = event.target.value;
+
+  }
+
+  useEffect(() => {
+    API.search()
+      .then((res) => {
+        setEmployees({ ...employees, users: res.data.results });
+      });
+  }, []);
+
+  const Rows = employees.users.map((user) => {
+    return (
+      <tr>
+        <td><img src={user.picture.thumbnail} alt={user.name.first}></img></td>
+        <td>{user.name.first + " " + user.name.last}</td>
+        <td>{user.email}</td>
+        <td>{user.phone}</td>
+        <td>{user.dob.date}</td>
+      </tr>
+    );
+
+  });
+
   return (
     <div>
       <Jumbotron />
-      <SearchBar />
+      <SearchBar handleInputChange={handleInputChange} />
+      <table class="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">DOB</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Rows}
+        </tbody>
+      </table>
     </div>
 
   )
